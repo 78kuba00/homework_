f7adf44a88031093bef85cb9b6ea3d318ce8a500
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-07 20:45:00 +0600

    Realised task list browse

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from webapp.models import Task
+
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
+
 
 # Create your views here.
